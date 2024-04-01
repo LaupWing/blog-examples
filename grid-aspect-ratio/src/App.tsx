d20ba9eb023b941f09cf73366ec829f99c1eba80
laupwing <loc_strict@hotmail.com>
2024-04-01 16:22:45 +0200

    Refactor grid layout and add dynamic background images

--- grid-aspect-ratio/src/App.tsx
+++ grid-aspect-ratio/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
     return (
-        <div className="grid p-10 bg-slate-600 gap-10 grid-cols-3">
-            <div className="aspect-square bg-red-300"></div>
-            <div className="aspect-square bg-red-300"></div>
-            <div className="aspect-square bg-red-300"></div>
+        <div className="grid grid-cols-1 md:grid-cols-3 p-10 bg-slate-600 gap-10">
+            <div className="aspect-square bg-[url(https://source.unsplash.com/random/500x500)]"></div>
+            <div className="aspect-square bg-[url(https://source.unsplash.com/random/500x500)]"></div>
+            <div className="aspect-square bg-[url(https://source.unsplash.com/random/500x500)]"></div>
         </div>
     )
 }
