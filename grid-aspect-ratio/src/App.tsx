23f0ccec336272cf20a0908937b5a9998f558d02
laupwing <loc_strict@hotmail.com>
2024-04-01 16:23:31 +0200

    Update aspect-square background images

--- grid-aspect-ratio/src/App.tsx
+++ grid-aspect-ratio/src/App.tsx
@@ -2,8 +2,8 @@ function App() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 p-10 bg-slate-600 gap-10">
             <div className="aspect-square bg-[url(https://source.unsplash.com/random/500x500)]"></div>
-            <div className="aspect-square bg-[url(https://source.unsplash.com/random/500x500)]"></div>
-            <div className="aspect-square bg-[url(https://source.unsplash.com/random/500x500)]"></div>
+            <div className="aspect-square bg-[url(https://source.unsplash.com/random/600x500)]"></div>
+            <div className="aspect-square bg-[url(https://source.unsplash.com/random/700x500)]"></div>
         </div>
     )
 }
