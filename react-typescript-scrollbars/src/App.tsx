5976441646fcd6747098465d639633c788e58e57
laupwing <loc_strict@hotmail.com>
2024-04-14 10:07:32 +0200

    Refactor scrollbar styles and layout in App.tsx

--- react-typescript-scrollbars/src/App.tsx
+++ react-typescript-scrollbars/src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
     return (
-        <main className="fixed inset-0 bg-slate-900">
-            <div className=""></div>
+        <main className="w-screen h-screen bg-slate-900 flex justify-center items-center">
+            <div className="scrollbar scrollbar-thumb-sky-700 scrollbar-track-sky-300 h-32 overflow-y-scroll">
+                <div className="h-64 flex w-10"></div>
+            </div>
+            <div className="scrollbar scrollbar-thumb-sky-700 scrollbar-track-emerald-300 h-32 overflow-y-scroll">
+                <div className="h-64 flex w-10"></div>
+            </div>
+            <div className="scrollbar scrollbar-thumb-emerald-600 scrollbar-track-emerald-300 h-32 overflow-y-scroll">
+                <div className="h-64 flex w-10"></div>
+            </div>
+            <div className="scrollbar scrollbar-thumb-emerald-600 scrollbar-track-emerald-300 h-32 scrollbar-thumb-rounded-full overflow-y-scroll">
+                <div className="h-64 flex w-10"></div>
+            </div>
         </main>
     )
 }
