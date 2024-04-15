591e695f88a7c307bd6f32b661f359eba6a964bc
laupwing <loc_strict@hotmail.com>
2024-04-15 14:08:52 +0200

    Update CKEditor component and dependencies in ck.tsx

--- ckeditor/src/app/_components/ck.tsx
+++ ckeditor/src/app/_components/ck.tsx
@@ -1,12 +1,29 @@
 import React from "react"
-import { CKEditor } from "@ckeditor/ckeditor5-react"
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { useMultiRootEditor } from "@ckeditor/ckeditor5-react"
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { MultiRootEditor } from "@ckeditor/ckeditor5-editor-multi-root"
+
+const multiRootEditorData = {
+    intro:
+        "<h2>Sample</h2><p>This is an instance of the " +
+        '<a href="https://ckeditor.com/docs/ckeditor5/latest/builds/guides/overview.html#classic-editor">multi-root editor build</a>.</p>',
+    content: "<p>This is some example content</p>",
+    outro:
+        "<p>You can use this sample to validate whether your " +
+        '<a href="https://ckeditor.com/docs/ckeditor5/latest/builds/guides/development/custom-builds.html">custom build</a> works fine.</p>',
+}
 
 const Ck = () => {
+    const editorConfig = {
+        editor: MultiRootEditor,
+
+        onReady: () => {},
+    }
+
     return (
         <div>
             <div>Ck</div>
-            <CKEditor
+            {/* <CKEditor
                 editor={ClassicEditor}
                 data="<p>Hello from CKEditor&nbsp;5!</p>"
                 onReady={(editor) => {
@@ -22,7 +39,7 @@ const Ck = () => {
                 onFocus={(event, editor) => {
                     console.log("Focus.", editor)
                 }}
-            />
+            /> */}
         </div>
     )
 }
